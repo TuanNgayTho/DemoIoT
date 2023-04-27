b0f76dee9b92e7d1c40773dc0e910c233ab0fcbf
TuanNgayTho <thienthancuto@gmail.com>
2023-04-27 13:52:52 +0700

    27/04/23

--- Led/tasks.py
+++ Led/tasks.py
@@ -17,28 +17,30 @@ def get_joke():
     # async_to_sync(channel_layer.group_send)('Mqtt', {'type': 'send_mqtt', 'text': mqtt_message})
 @shared_task
 def mpqtt_message():
-    class MyMQTTClass(mqtt.Client):
+    pass
 
-        def on_connect(self, mqttc, obj, flags, rc):
-            print("rc: " + str(rc))
+class MyMQTTClass(mqtt.Client):
 
-        def on_connect_fail(self, mqttc, obj):
-            print("Connect failed")
+    def on_connect(self, mqttc, obj, flags, rc):
+        print("rc: " + str(rc))
 
-        def on_message(self, mqttc, obj, msg):
-            print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
-            mqtt_message = str(msg.payload)
-            async_to_sync(channel_layer.group_send)('Mqtt', {'type': 'send_mqtt', 'text': mqtt_message})
+    def on_connect_fail(self, mqttc, obj):
+        print("Connect failed")
 
-        def run(self):
-            self.connect("103.184.113.154", 1883, 60)
-            self.subscribe("Test", 0)
+    def on_message(self, mqttc, obj, msg):
+        print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+        mqtt_message = str(msg.payload)
+        async_to_sync(channel_layer.group_send)('Mqtt', {'type': 'send_mqtt', 'text': mqtt_message})
 
-            rc = 0
-            while rc == 0:
-                rc = self.loop()
-            return rc
+    def run(self):
+        self.connect("103.184.113.154", 1883, 60)
+        self.subscribe("Test", 0)
 
-    mqttc = MyMQTTClass()
-    p1 = threading.Thread(target=mqttc.run, daemon= True, args=())
-    p1.start()
+        rc = 0
+        while rc == 0:
+            rc = self.loop()
+        return rc
+
+mqttc = MyMQTTClass()
+p1 = threading.Thread(target=mqttc.run, daemon= True, args=())
+p1.start()
