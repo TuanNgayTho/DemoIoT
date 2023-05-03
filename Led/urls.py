e37c53d9f0cf138b95bcd77ef9c53fc1480d7556
TuanNgayTho <thienthancuto@gmail.com>
2023-05-03 11:06:47 +0700

    03/05/23

--- Led/urls.py
+++ Led/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import index
+from Led import views
 
+app_name = "loginpage"
 urlpatterns = [
-    path('', index),
+    path('login', views.loginpage, name='login'),
+    path('logout', views.logoutpage, name='logout'),
+    path('home', views.home, name='home'),
+    path('homepage', views.homepage.as_view(), name="homepage")
 ]
---
+++ static/js/app.js
@@ -0,0 +1,32 @@
+//menu navbar
+function tuanthegioi()
+{
+    var x = document.getElementById("navbar");
+    if (x.className === "globalmenu")
+        {x.className += " tuan";}
+    else
+        {x.className = "globalmenu";}
+}
+//menu navbar end block
+
+//websocket
+var socket = new WebSocket('ws://localhost:8000/ws/Led/');
+socket.onmessage = function(event){
+var joke = event.data;
+console.log("Message from server ", event.data);
+document.querySelector('#jokes').innerText = joke;
+}
+
+var socket = new WebSocket('ws://localhost:8000/ws/Mqtt/');
+socket.onmessage = function(event){
+const mqtt = event.data;
+console.log(event.data);
+document.querySelector('#mqtt').innerText = mqtt;
+
+const massage = JSON.parse(mqtt)
+for (let x in massage) {
+    console.log(massage[x]);
+    document.querySelector('#'+x).innerText = massage[x];
+    }
+}
+//websocket end block
